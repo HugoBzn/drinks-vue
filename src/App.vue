86bafd94e9f619e122b4dce51b1d230a0f70f7eb
HugoBzn <hugobazan1499@gmail.com>
2023-07-04 14:04:57 -0600

    Add two pages and classes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
+import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
 </script>
 
 <template>
   <div>
     <Header />
+
+    <main class="container mx-auto mt-10">
+      <RouterView />
+    </main>
   </div>
 </template>
-
-<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-6xl font-extrabold">Recetas</h1>
+  </div>
+</template>
