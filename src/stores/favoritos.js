ca9d04a3f981d1c7ee1cedc05f523457ac936b87
HugoBzn <hugobazan1499@gmail.com>
2023-07-05 20:27:47 -0600

    Prevent duplicate elements favorites

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -20,13 +20,22 @@ export const useFavoritosStore = defineStore('favoritos', () => {
     }
   );
 
-  const sincronizarLocalStorage = () => {
+  function sincronizarLocalStorage() {
     localStorage.setItem('favoritos', JSON.stringify(favoritos.value));
-  };
+  }
 
-  const handleClickFavorito = () => {
-    favoritos.value.push(bebidas.receta);
-  };
+  function existeFavorito(id) {
+    const favoritosLocalStorage = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+    return favoritosLocalStorage.some((favorito) => favorito.idDrink === id);
+  }
+
+  function handleClickFavorito() {
+    if (existeFavorito(bebidas.receta.idDrink)) {
+      console.log('This item already exist on favorites');
+    } else {
+      favoritos.value.push(bebidas.receta);
+    }
+  }
 
   return {
     favoritos,
